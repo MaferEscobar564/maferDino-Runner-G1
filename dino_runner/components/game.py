a2afaa90427c3844e2c66c3443cd316a79584f35
MaferEscobar564 <126780244+MaferEscobar564@users.noreply.github.com>
2023-03-07 21:35:46 -0400

    Clase 3

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,34 +1,44 @@
 import pygame
-import dino_runner
-from dino_runner.components.dinosaur import Dinosaur
-from dino_runner.components.obstacles.cactus import SmallCactus, LargeCactus
-from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.score import Score
 
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, FONT_STYLE, DINO_START
+from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 class Game:
-    def __init__(self):
+    def _init_(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
+        self.executing = False
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-        
+        self.death_count = 0
+
         self.player = Dinosaur()
         self.manager = ObstacleManager()
+        self.score = Score()
 
     def run(self):
+        self.executing = True
+        while self.executing:
+            if not self.playing:
+                self.show_menu()
+
+        pygame.quit()
+
+    def start_game(self):
         # Game loop: events - update - draw
         self.playing = True
+        self.manager.reset()
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
@@ -36,19 +46,20 @@ class Game:
                 self.playing = False
 
     def update(self):
-        user_input = pygame.key.get_pressed()
-        self.player.update(user_input)
-        self.manager.update(self.game_speed, self.player, self)
+        user_imput = pygame.key.get_pressed()
+        self.player.update(user_imput)
+        self.manager.update(self.game_speed,self.player,self.on_death)
+        self.score.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255)) 
+        self.screen.fill((255, 255, 255))
         self.draw_background()
-        self.player.draw(self.screen)
         self.manager.draw(self.screen)
-        #pygame.display.update()
+        self.player.draw(self.screen)
+        self.score.draw(self.screen)
+        pygame.display.update()
         pygame.display.flip()
-        
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -58,3 +69,40 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+    def on_death(self):
+        self.playing = False
+        self.death_count +=1
+
+    def show_menu(self):
+        #Llenar pantalla en blanco 
+        self.screen.fill((255,255,255))
+        #Mensaje de bienvenida centrado
+        half_screen_width = SCREEN_WIDTH // 2
+        half_screen_height = SCREEN_HEIGHT // 2
+        if not self.death_count:
+            font = pygame.font.Font(FONT_STYLE, 32)
+            text = font.render("Welcome, press any key to start!",True,(0,0,0))
+            text_rect = text.get_rect()
+            text_rect.center = (half_screen_width, half_screen_height)
+            self.screen.blit(text, text_rect)
+        else:
+            #Mostrar mensaje para que reinicie,cuantas veces muere y el puntaje
+            pass
+            #Icono al centro
+        self.screen.blit(DINO_START,(half_screen_width -
+                                    40, half_screen_height - 140))
+
+        #Plasmar cambios
+        pygame.display.update()
+        #Manejar los eventos
+        self.handle_menu_events()
+
+    def handle_menu_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.executing = False
+            
+            elif event.type == pygame.KEYDOWN:
+                self.start_game()
